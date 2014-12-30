070ff2dc038e2b54fac1a2d94d00838187c4f8dc
Raphael <raphael@zenhausern.net>
2014-12-30 23:17:50 +0100

    Fixing wrong file commit

--- digOres.lua
+++ digOres.lua
@@ -6,172 +6,92 @@ local back  = "back"
 local right = "right"
 local left = "left"
 
+-- ------------------------------------------------------------ --
+-- START OF THE CODE THAT GETS EXECUTED WHEN THE PROGRAM STARTS --
+-- ------------------------------------------------------------ --
+
 -- LOAD FUNCTIONS FROM THE OTHER FILES
+os.loadAPI("api_digOres")
 os.loadAPI("api_sharedFunctions")
 os.loadAPI("api_turtleExt")
 
--- ------------------------------------------ --
--- START OF THE FUNCTIONS SPECIFIC TO digOres --
--- ------------------------------------------ --
+-- GET THE CONFIGURATION OF THE PROGRAM
+local args={...}
+local configuration = api_sharedFunctions.getConfig("config", args)
+if configuration == nil then
+  return
+end
 
--- EXCAVATES A SHAFT, DIGGING OUT ALL THE SPECIAL BLOCKS (ORES ETC)
-function excavateShaft(configuration, dir)
-  local squaresMoved=0
-  for i=1,configuration.centerRadius+api_sharedFunctions.calculateMoves(configuration, configuration.currentL, 0, configuration.currentS) do
-    if api_sharedFunctions.needsRestocking(configuration) then
-      api_turtleExt.turnTo(back)
-      exitShaftAndRestock(configuration, dir, squaresMoved)
-    end
-    if api_turtleExt.digAndMove(forward)==0 then
-      api_sharedFunctions.reportObstruction(configuration, dir)
-      break
-    end
-    squaresMoved = squaresMoved + 1
-    checkSides(configuration, up) 
+-- USED TO KEEP TRACK OF THE CURRENT POSITION
+configuration.currentL = 1
+configuration.currentT = 1
+configuration.currentS = 0
+
+-- USED TO KEEP TRACK OF THE NUMBER OF EXCAVATED ORES
+configuration.numOres = 0
+
+-- CHECK THE MINIMAL FUEL VALUE SPECIFIED BY THE USER
+if (configuration.departureFuel < 400) then
+  print("Warning: Low fuel value chosen")
+  print("Do you wish to proceed? (Y/N)")
+  local answer=io.read()
+  if not (answer=="Y") then
+    return
   end
-  api_turtleExt.digAndMove(down, 1, 0)
-  api_turtleExt.turnTo(back)
-  checkSides(configuration, down)
-  for i=1,squaresMoved do
-    if configuration.placeTorches and (squaresMoved%6==5) then
-      api_turtleExt.place(up, 1)
-    end
-    if api_sharedFunctions.needsRestocking(configuration) then
-      api_turtleExt.digAndMove(up, 1, 0)
-      exitShaftAndRestock(configuration, dir, squaresMoved)
-      api_turtleExt.turnTo(back)
-      api_turtleExt.digAndMove(down, 1, 0)
-    end
-    if api_turtleExt.digAndMove(forward)==0 then
-      api_sharedFunctions.reportObstruction(configuration, dir)
-      break
-    end
-    squaresMoved = squaresMoved - 1
-    checkSides(configuration, down) 
+end
+
+-- CHECK THE NUMBER OF BLOCKS PROVIDED BY THE USER
+api_sharedFunctions.dropoffAndRestock(configuration, configuration.numIgnoreBlocks, true, true, true)
+if not api_digOres.enoughBlocksProvided(configuration) then
+  term.clear()
+  print("ERROR:")
+  print("-------------------------------")
+  print("You said you wanted to turtle")
+  print("To ignore "..configuration.numIgnoreBlocks.." blocks but didn't")
+  print("place them all in the chest")
+  print("chest direction="..configuration.ignoreDir)
+  print("-------------------------------")
+  print("ABORTING")
+  print("")
+  print("")
+  print("")
+  print("")
+  api_sharedFunctions.dropoff(configuration, configuration.numIgnoreBlocks, true, true)
+  return
+end
+
+-- FOR EVERY LEVEL
+for curL = 1 + configuration.skipL, configuration.numL do
+  local firstT = 1
+  if curL == 1 + configuration.skipL then
+    firstT = firstT + configuration.skipT
   end
-  api_turtleExt.digAndMove(up, 1, 0)
-  api_turtleExt.digAndMove(forward, squaresMoved, 0)
-end
-
--- CHECKS IF THE BLOCKS AT THE LEFT OR RIGHT ARE SPECIAL (AND THE TOP OR BOTTOM, DEPENDING ON THE INPUT) 
--- IF SO IT WILL DIG OUT THAT BLOCK AND EXCAVATE THE REST OF THE VEIN
-function checkSides(configuration, vDir)
-  check(configuration, vDir)
-  check(configuration, left)
-  check(configuration, right)
-end
-
--- DROPS THE ITEMS IN THE INVENTORY AND REFUELS THE TURTLE
-function exitShaftAndRestock(configuration, dir, squaresMoved)
-  api_turtleExt.digAndMove(forward, squaresMoved, 0)
-  api_turtleExt.turnTo(dir)
-  api_sharedFunctions.dropoffAndRestock(configuration, configuration.numIgnoreBlocks, false, false, false)
-  api_turtleExt.turnTo(dir)
-  api_turtleExt.digAndMove(forward, squaresMoved, 0)
-end
-
--- CHECKS IF A BLOCK IN A CERTAIN DIRECTION IS SPECIAL
--- IF SO IT WILL DIG OUT THAT BLOCK AND EXCAVATE THE REST OF THE VEIN
-function check(configuration, dir)
-  api_turtleExt.turnTo(dir)
-  local tDir=api_turtleExt.turnedDir(dir)
-  if api_turtleExt.detect(tDir) then
-    if isSpecial(configuration, tDir) then
-      if api_turtleExt.digAndMove(tDir) == 1 then
-        configuration.numOres=configuration.numOres+1
-        excavate(configuration)
-        api_turtleExt.digAndMove(api_turtleExt.reverseDir(tDir), 1, 0)
+  -- FOR EVERY TUNNEL
+  for curT = firstT, configuration.numT do
+    local firstS = 1
+    if (curL == 1 + configuration.skipL) and (curT == 1 + configuration.skipT) and (configuration.skipS~=0) then
+      firstS = 1 + configuration.skipS
+    end
+    -- FOR EVERY SHAFT
+    for curS = firstS, configuration.numS do
+      api_sharedFunctions.moveToLocation(configuration, curL, curT, curS)
+      api_turtleExt.turnTo(left)
+      api_digOres.excavateShaft(configuration, left)
+      api_digOres.excavateShaft(configuration, right)
+      api_turtleExt.turnTo(right)
+      if api_sharedFunctions.needsRestocking(configuration) then
+        api_sharedFunctions.dropoffAndRestock(configuration, 0, false, false, false)
       end
     end
   end
-  api_turtleExt.turnFrom(dir)
 end
 
--- CHECKS IF A BLOCK IN A CERTAIN DIRECTION IS SPECIAL (NOT ONE OF THE REFERENCE COMPONENTS (DIRT ETC))
-function isSpecial(configuration, dir)
-  api_turtleExt.turnTo(dir)
-  local tDir=api_turtleExt.turnedDir(dir)
-  local torchSlot=0
-  if configuration.placeTorches then
-    torchSlot=1
-  end
-  if not api_turtleExt.detect(tDir) then
-    return false
-  end
-  for i=1, configuration.numIgnoreBlocks+torchSlot do
-    if api_turtleExt.compare(tDir, i) then
-      api_turtleExt.turnFrom(dir)
-      return false
-    end
-  end
-  api_turtleExt.turnFrom(dir)
-  return true
-end
+-- RETURN TO START AND DROPOFF
+api_sharedFunctions.moveToLocation(configuration, 1, 1, 0)
+api_sharedFunctions.dropoff(configuration, configuration.numIgnoreBlocks, true, true)
+print("Execution finished")
+print("Number of ores dug: "..configuration.numOres)
 
--- EXCAVATES AN ORE VEIN
-function excavate(configuration)
-  local numSteps = 0
-  local steps = {}
-  repeat
-    steps[numSteps] = steps[numSteps] or 0
-    steps[numSteps] = lookForOres(configuration, steps[numSteps])  
-    if steps[numSteps] ~= 7 then
-      if ((numSteps==0) or (steps[numSteps]~=api_turtleExt.reverseIntDir(steps[numSteps-1]))) then
-        if api_turtleExt.digAndMove(api_turtleExt.turnedDir(api_turtleExt.intToDir(steps[numSteps]))) == 1 then
-          configuration.numOres=configuration.numOres+1
-          numSteps = numSteps + 1
-        end
-      end
-    else
-      steps[numSteps] = 0
-      numSteps = numSteps - 1
-      if numSteps >= 0 then
-        api_turtleExt.digAndMove(api_turtleExt.reverseDir(api_turtleExt.turnedDir(api_turtleExt.intToDir(steps[numSteps]))), 1, 0)
-      end
-    end
-  until numSteps < 0
-end
-
--- LOOKS AROUND THE CURRENT POSITION, LOOKING FOR ORES
-function lookForOres(configuration, dir)
-  if dir < 1 then
-    dir = dir + 1
-    if isSpecial(configuration, up) then
-      return 1
-    end
-  end
-  if dir < 2 then
-    dir = dir + 1
-    if isSpecial(configuration, down) then
-      return 2
-    end
-  end
-  if dir > 2 then
-    api_turtleExt.turnTo(left)
-  end
-  for dir=dir+1, 6 do
-    if isSpecial(configuration, forward) then
-      return dir
-    end
-    api_turtleExt.turnTo(left)
-  end
-  return 7
-end
-
--- CHECKS IF THE USER PLACED ENOUGH BLOCKS IN THE INVENTORY
-function enoughBlocksProvided(configuration)
-  local torchSlot=0
-  if configuration.placeTorches then
-    torchSlot=1
-  end
-  for i=1+torchSlot,configuration.numIgnoreBlocks+torchSlot do
-    if turtle.getItemCount(i)==0 then
-      return false
-    end
-  end
-  return true
-end
-
--- ---------------------------------------- --
--- END OF THE FUNCTIONS SPECIFIC TO digOres --
--- ---------------------------------------- --
+-- ---------------------------------------------------------- --
+-- END OF THE CODE THAT GETS EXECUTED WHEN THE PROGRAM STARTS --
+-- ---------------------------------------------------------- --
