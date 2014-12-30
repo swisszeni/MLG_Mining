6f0a68dbc0bb877c57511db459baed982b62abc9
Raphael <raphael@zenhausern.net>
2014-12-30 23:20:45 +0100

    Fixed file-content-turn

--- digShafts.lua
+++ digShafts.lua
@@ -6,32 +6,75 @@ local back  = "back"
 local right = "right"
 local left = "left"
 
+-- ------------------------------------------------------------ --
+-- START OF THE CODE THAT GETS EXECUTED WHEN THE PROGRAM STARTS --
+-- ------------------------------------------------------------ --
+
 -- LOAD FUNCTIONS FROM THE OTHER FILES
+os.loadAPI("api_digShafts")
 os.loadAPI("api_sharedFunctions")
 os.loadAPI("api_turtleExt")
 
--- -------------------------------------------- --
--- START OF THE FUNCTIONS SPECIFIC TO digShafts --
--- -------------------------------------------- --
-
--- DIGS A SINGLE SHAFT, THE PARAMETERS WILL DETERMINE THE LENGTH OF THE SHAFT
-function digShaft(configuration, dir)
-  local squaresMoved=0
-  for i=1,configuration.centerRadius+api_sharedFunctions.calculateMoves(configuration, configuration.currentL, 0, configuration.currentS) do
-    if configuration.placeTorches and (i % 6==5) then
-      api_turtleExt.place(down, 1)
-    end
-    if api_turtleExt.digAndMove(forward)==0 then
-      api_sharedFunctions.reportObstruction(configuration, dir)
-      break
-    end
-    squaresMoved = squaresMoved + 1
-    api_turtleExt.dig(down)
-  end
-  api_turtleExt.turnTo(back)
-  api_turtleExt.digAndMove(forward, squaresMoved, 0)
+-- GET THE CONFIGURATION OF THE PROGRAM
+local args={...}
+local configuration = api_sharedFunctions.getConfig("config", args)
+if configuration == nil then
+  return
 end
 
--- ------------------------------------------ --
--- END OF THE FUNCTIONS SPECIFIC TO digShafts --
--- ------------------------------------------ --
+-- USED TO KEEP TRACK OF THE CURRENT POSITION
+configuration.currentL = 1
+configuration.currentT = 1
+configuration.currentS = 0
+
+-- CHECK THE MINIMAL FUEL VALUE SPECIFIED BY THE USER
+if (configuration.departureFuel < 400) then
+  print("Warning: Low fuel value chosen")
+  print("Do you wish to proceed? (Y/N)")
+  local answer=io.read()
+  if not (answer=="Y") then
+    return
+  end
+end
+
+-- RESTOCK THE TURTLE
+api_sharedFunctions.dropoffAndRestock(configuration, 0, false, true, true)
+
+-- FOR EVERY LEVEL
+for curL = 1 + configuration.skipL, configuration.numL do
+  local firstT = 1
+  if curL == 1 + configuration.skipL then
+    firstT = firstT + configuration.skipT
+  end
+  -- FOR EVERY TUNNEL
+  for curT = firstT, configuration.numT do
+    local firstS = 1
+    if (curL == 1 + configuration.skipL) and (curT == 1 + configuration.skipT) and (configuration.skipS~=0) then
+      firstS = 1 + configuration.skipS
+    end
+    -- FOR EVERY SHAFT
+    for curS = firstS, configuration.numS do
+      api_sharedFunctions.moveToLocation(configuration, curL, curT, curS)
+      api_turtleExt.turnTo(left)
+      api_digShafts.digShaft(configuration, left)
+      if api_sharedFunctions.needsRestocking(configuration) then
+        api_turtleExt.turnTo(left)
+        api_sharedFunctions.dropoffAndRestock(configuration, 0, false, false, false)
+        api_turtleExt.turnTo(right)
+      end
+      api_digShafts.digShaft(configuration, right)
+      api_turtleExt.turnTo(right)
+      if api_sharedFunctions.needsRestocking(configuration) then
+        api_sharedFunctions.dropoffAndRestock(configuration, 0, false, false, false)
+      end
+    end
+  end
+end
+
+-- RETURN TO START AND DROPOFF
+api_sharedFunctions.moveToLocation(configuration, 1, 1, 0)
+api_sharedFunctions.dropoff(configuration, 0, true, true)
+
+-- ---------------------------------------------------------- --
+-- END OF THE CODE THAT GETS EXECUTED WHEN THE PROGRAM STARTS --
+-- ---------------------------------------------------------- --
